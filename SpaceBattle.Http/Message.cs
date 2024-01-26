39a7d2f75d02759a397c76fd803f476dc38101d9
Aleshua <snleha19@gmail.com>
2024-01-26 12:48:22 +0600

    chages

--- SpaceBattle.Http/Message.cs
+++ SpaceBattle.Http/Message.cs
@@ -3,11 +3,11 @@ namespace SpaceBattle.Http;
 [DataContract(Name = "Message")]
 public class Message {
     [DataMember(Name = "SimpleProperty", Order = 1)]
-    public string GameID { get; set; }
+    public required string GameID { get; set; }
 
     [DataMember(Name="CommandName")]
-    public string CommandName { get; set; }
+    public required string CommandName { get; set; }
 
     [DataMember(Name="args")]
-    public Dictionary<string, string> args { get; set; }
+    public Dictionary<string, string> args { get; set; } = new Dictionary<string, string>();
 }
