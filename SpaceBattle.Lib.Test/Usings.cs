bf9d8893367295bd14d66ea5b20e6dc2bbc388b6
bezuglovdenis <bezuglov200395@gmail.com>
2024-05-10 23:23:18 +0600

    update2

---
+++ SpaceBattle.Lib.Test/Usings.cs
@@ -0,0 +1,10 @@
+global using Xunit;
+global using Moq;
+global using Hwdtech;
+global using Hwdtech.Ioc;
+global using Xunit;
+global using System.Net;
+global using Microsoft.AspNetCore;
+global using Microsoft.AspNetCore.Hosting;
+global using System.Collections.Concurrent;
+global using System.Threading
