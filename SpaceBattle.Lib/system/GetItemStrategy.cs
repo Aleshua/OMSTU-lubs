5f3bc36af34f838af94b8da5f653e75ffbe2a2c4
bezuglovdenis <bezuglov200395@gmail.com>
2024-05-19 22:17:31 +0600

    small fix

--- SpaceBattle.Lib/system/GetItemStrategy.cs
+++ SpaceBattle.Lib/system/GetItemStrategy.cs
@@ -8,7 +8,7 @@ public class GetItemStrategy : IStrategy
     public object Execute(params object[] args)
     {
         Dictionary<string, IUObject> objects = IoC.Resolve<Dictionary<string, IUObject>>("Get.Objects");
-        objects.TryGetValue((string) args[0], out IUObject obj);
+        objects.TryGetValue((string) args[0], out IUObject? obj);
 
         if (obj != null) return obj;
         throw new Exception();
